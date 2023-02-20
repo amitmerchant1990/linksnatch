08ebce3d3372bf9b0b4cddc5fc4dbd4a0067a8fd
Amit Merchant <bullredeyes@gmail.com>
2023-02-20 13:20:28 +0530

    fix an issue related to switching themes.

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,93 +1,98 @@
 import { publicRuntimeConfig } from "next.config"
 import Image from "next/image"
 import Link from "next/link"
+import { useEffect } from "react"
 
 export function AppHeader() {
-    if (typeof window !== 'undefined') {
-        const HSThemeAppearance = {
-            init() {
-                const defaultTheme = 'default'
-                let theme = localStorage.getItem('hs_theme') || defaultTheme
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const HSThemeAppearance = {
+                init() {
+                    const defaultTheme = 'default'
+                    let theme = localStorage.getItem('hs_theme') || defaultTheme
 
-                if (document.querySelector('html').classList.contains('dark')) return
-                this.setAppearance(theme)
-            },
-            _resetStylesOnLoad() {
-                const $resetStyles = document.createElement('style')
-                $resetStyles.innerText = `*{transition: unset !important;}`
-                $resetStyles.setAttribute('data-hs-appearance-onload-styles', '')
-                document.head.appendChild($resetStyles)
-                return $resetStyles
-            },
-            setAppearance(theme, saveInStore = true, dispatchEvent = true) {
-                const $resetStylesEl = this._resetStylesOnLoad()
+                    if (document.querySelector('html').classList.contains('dark')) return
+                    this.setAppearance(theme)
+                },
+                _resetStylesOnLoad() {
+                    const $resetStyles = document.createElement('style')
+                    $resetStyles.innerText = `*{transition: unset !important;}`
+                    $resetStyles.setAttribute('data-hs-appearance-onload-styles', '')
+                    document.head.appendChild($resetStyles)
+                    return $resetStyles
+                },
+                setAppearance(theme, saveInStore = true, dispatchEvent = true) {
+                    const $resetStylesEl = this._resetStylesOnLoad()
 
-                if (saveInStore) {
-                    localStorage.setItem('hs_theme', theme)
-                }
+                    if (saveInStore) {
+                        localStorage.setItem('hs_theme', theme)
+                    }
 
-                if (theme === 'auto') {
-                    theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
-                }
+                    if (theme === 'auto') {
+                        theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
+                    }
 
-                document.querySelector('html').classList.remove('dark')
-                document.querySelector('html').classList.remove('default')
-                document.querySelector('html').classList.remove('auto')
+                    document.querySelector('html').classList.remove('dark')
+                    document.querySelector('html').classList.remove('default')
+                    document.querySelector('html').classList.remove('auto')
 
-                document.querySelector('html').classList.add(theme)
+                    document.querySelector('html').classList.add(theme)
 
-                setTimeout(() => {
+                    setTimeout(() => {
+                        $resetStylesEl.remove()
+                    })
+
+                    if (dispatchEvent) {
+                        window.dispatchEvent(new CustomEvent('on-hs-appearance-change', { detail: theme }))
+                    }
+
                     $resetStylesEl.remove()
+                },
+                getAppearance() {
+                    let theme = this.getOriginalAppearance()
+                    if (theme === 'auto') {
+                        theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
+                    }
+                    return theme
+                },
+                getOriginalAppearance() {
+                    const defaultTheme = 'default'
+                    return localStorage.getItem('hs_theme') || defaultTheme
+                }
+            }
+
+            HSThemeAppearance.init()
+
+            window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+                if (HSThemeAppearance.getOriginalAppearance() === 'auto') {
+                    HSThemeAppearance.setAppearance('auto', false)
+                }
+            })
+
+            const $clickableThemes = document.querySelectorAll('[data-hs-theme-click-value]')
+            const $switchableThemes = document.querySelectorAll('[data-hs-theme-switch]')
+
+            $clickableThemes.forEach(function ($item) {
+                $item.addEventListener('click', function () {
+                    HSThemeAppearance.setAppearance($item.getAttribute('data-hs-theme-click-value'), true, $item)
+                })
+            })
+
+            $switchableThemes.forEach($item => {
+                $item.addEventListener('change', (e) => {
+                    HSThemeAppearance.setAppearance(e.target.checked ? 'dark' : 'default')
                 })
 
-                if (dispatchEvent) {
-                    window.dispatchEvent(new CustomEvent('on-hs-appearance-change', { detail: theme }))
-                }
+                $item.checked = HSThemeAppearance.getAppearance() === 'dark'
+            })
 
-                $resetStylesEl.remove()
-            },
-            getAppearance() {
-                let theme = this.getOriginalAppearance()
-                if (theme === 'auto') {
-                    theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
-                }
-                return theme
-            },
-            getOriginalAppearance() {
-                const defaultTheme = 'default'
-                return localStorage.getItem('hs_theme') || defaultTheme
-            }
+            window.addEventListener('on-hs-appearance-change', e => {
+                $switchableThemes.forEach($item => {
+                    $item.checked = e.detail === 'dark'
+                })
+            })
         }
-
-        HSThemeAppearance.init()
-
-        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-            if (HSThemeAppearance.getOriginalAppearance() === 'auto') {
-                HSThemeAppearance.setAppearance('auto', false)
-            }
-        })
-
-        const $clickableThemes = document.querySelectorAll('[data-hs-theme-click-value]')
-        const $switchableThemes = document.querySelectorAll('[data-hs-theme-switch]')
-
-        $clickableThemes.forEach($item => {
-            $item.addEventListener('click', () => HSThemeAppearance.setAppearance($item.getAttribute('data-hs-theme-click-value'), true, $item))
-        })
-
-        $switchableThemes.forEach($item => {
-            $item.addEventListener('change', (e) => {
-                HSThemeAppearance.setAppearance(e.target.checked ? 'dark' : 'default')
-            })
-
-            $item.checked = HSThemeAppearance.getAppearance() === 'dark'
-        })
-
-        window.addEventListener('on-hs-appearance-change', e => {
-            $switchableThemes.forEach($item => {
-                $item.checked = e.detail === 'dark'
-            })
-        })
-    }
+    }, [])
 
     return (
         <header class="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
