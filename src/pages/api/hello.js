857f96ab53c868d5cea88b7f344f1594daa1ff4b
Amit Merchant <bullredeyes@gmail.com>
2025-03-18 22:51:00 +0530

    remove redundant code.

--- src/pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export const config = {
-  runtime: 'edge',
-}
-
-export default async function (req) {
-  return Response.json({ name: 'John Doe' })
-}
